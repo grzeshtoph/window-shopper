677ea8a1239b1e0f1175ff8bff74433526603d7f
greg.smorag <greg.smorag@gmail.com>
2017-12-12 21:47:01 +0000

    calculate function and unit test

---
+++ window-shopper/Controllers/MainVC.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Greg Smorag on 11/12/2017.
+//  Copyright © 2017 Greg Smorag. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+        print("We got here" )
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
